538c2a84b3e82a6b24a2bb53e49fd11a4273a070
Unknown <unknown@example.com>
2017-07-18 21:18:51 +0200

    ds

--- Exercices/10_nb_jours.js
+++ Exercices/10_nb_jours.js
@@ -1,3 +1,6 @@
 // alert("salut");
-var mois = "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12";
-var jour = 30;
+
+var mois = (prompt("Entrez un mois :"));
+if (mois || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9 || 10 || 11 || 12) {
+    document.write(1 + " janvier");
+}
